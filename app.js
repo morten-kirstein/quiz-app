5c9af60b9277dbafc860d14aa3baafba8ac23193
Morten Kirstein <info@mortenkirstein.dk>
2017-02-08 22:44:10 +0100

    Add eventListeners to prev & next button

--- app.js
+++ app.js
@@ -4,8 +4,44 @@ var quiz = [ 'quistion1', 'quistion3' , 'quistion3'];
 
 function initializeApp() {
    var app;
+
+   addButtonListeners();
+   
    var app = document.querySelector("#app");
    app.innerHTML = "";
+
+
+   /**
+    * @author HM/MKI
+    * @description Adds eventlisteners to prev & next button
+    */
+   function addButtonListeners() {
+        var prevbutton = document.querySelector('.prevBtn');
+        var nextbutton = document.querySelector('.nextBtn');
+    
+        prevbutton.addEventListener('click', prev);
+        nextbutton.addEventListener('click', next);
+   }
+
+   /**
+    *  @author HM/MKI
+    *  @description When user clicks previous button
+    */
+   function prev() {
+        console.log('prev');
+   }
+
+   /**
+    *  @author HM/MKI
+    *  @description When user clicks next button
+    */
+   function next() {
+        console.log('next');
+   }
+
+
+
+
 }
 
 
