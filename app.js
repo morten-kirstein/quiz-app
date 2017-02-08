ce526cafc0b7c56b321040a59457638e8db1b46b
Morten Kirstein <info@mortenkirstein.dk>
2017-02-08 22:28:28 +0100

    refactor

--- app.js
+++ app.js
@@ -1,13 +1,12 @@
-function main() {
+
    
+var quiz = [ 'quistion1', 'quistion3' , 'quistion3'];
+
+function initializeApp() {
+    debugger;
    var app;
    var app = document.querySelector("#app");
-
-   app.innerHTML = "hello world";
-
-
-
- 
+   app.innerHTML = "";
 }
 
 
