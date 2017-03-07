86b9435f3e194f82150ef8509f681bbf19ffc1f6
Morten Kirstein <info@mortenkirstein.dk>
2017-03-07 22:01:52 +0100

    refac to itterate over anwsers

--- app.js
+++ app.js
@@ -51,29 +51,74 @@ function initializeApp() {
 
         //Set questions in collection
         var quizQuestions = [];
+        var totalQuestions;
+        var currentQuestion = 0;
+
+        var quizTitle = document.querySelector('#question-title');
+        var quizAnwsers = document.querySelector('#question-anwsers');
 
         //Spørg firebase om der er nogle spørgsmål
         messageRef.on('child_added', function(snapshot) {
             quizQuestions.push(snapshot.val());
 
+            totalQuestions = quizQuestions.length;
+
             //display first question in collection
             var firstQuestion = quizQuestions[0];
-            showQuestion(firstQuestion.title);
+            showQuestion(firstQuestion);
 
         });
 
-
-        // InitializeCurrentQuestion
-        var currentQuestion = 0;
-        var totalQuestions = quizQuestions.length;
-
-        var app = document.querySelector('#app');
+       
         addButtonListeners();
 
         // Show the question and update UI
-        function showQuestion(questionNr){
-            app.innerHTML = questionNr;
+        function showQuestion(question){
+
+            var questionTitle = question.title;
+
+            var wrongAnswers = question.wrongAnswers;
+            var correctAnswers = question.correctAnswer;
+
+            var answersCollection = wrongAnswers.concat(correctAnswers);
+            
+            var answersTitles = [];
+
+
+            for(var i = 0; answersCollection.length > i; i++) {
+
+                answersTitles.push(answersCollection[i].title);
+                debugger;
+            } 
+            
+            
+        
+            debugger;
+            var anwserTemplate = makeAnwserHTMLTemplate(answersTitles);
+
+            quizTitle.innerHTML = questionTitle;
+            quizAnwsers.innerHTML = anwserTemplate;
+           
         }
+
+        
+        /**
+         * 
+         * @param {*} anwserCollection 
+         */
+        function makeAnwserHTMLTemplate(anwserCollection){
+
+            var title = "hello";
+
+            var htmlstring = [
+                '<ul">',
+                    '<li>' + title + '</li>',
+                '</ul>'
+            ].join("\n");
+
+            return htmlstring;
+        }
+
 
         // Add event listeners to buttons
         function addButtonListeners() {
